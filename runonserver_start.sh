115f3f382d90e942db8df8421377d761aaa7d607
pekas1969 <peter.kasparak@gmail.com>
2025-05-24 09:28:04 +0200

    feat: add server grouping feature and group-level global commands

--- runonserver_start.sh
+++ runonserver_start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# Starte das virtuelle Environment und die Anwendung
 
-if [ ! -d "runonserver-env" ]; then
-  echo "Virtuelles Environment 'runonserver-env' nicht gefunden. Bitte zuerst in der README.md das Environment anlegen."
-  python3 -m venv runonserver-env
-  #exit 1
-fi
-
-source runonserver-env/bin/activate
 python3 run_on_server.py
-deactivate
+
